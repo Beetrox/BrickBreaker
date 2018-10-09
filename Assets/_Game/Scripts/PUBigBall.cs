0e983ac6207077f212a17617be60983073d2b65e
Tora Lindberg <toto0181@hotmail.com>
2018-10-09 17:39:28 +0200

    changed script on power up prefabs

---
+++ Assets/_Game/Scripts/PUBigBall.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PUBigBall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Debug.Log("collided");
+        if (collision.gameObject.tag == "Paddle")
+        {
+            //Debug.Log("power up");
+            // connect to FastBall()
+            Destroy(gameObject, 0.05f);
+        }
+
+        if (collision.gameObject.tag == "Bottom")
+        {
+            //Debug.Log("bottom");
+            Destroy(gameObject);
+        }
+        //inputController.PowerUpFlash();
+    }
+}
